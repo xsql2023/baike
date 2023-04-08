3a78c1380bd194c64609fe0c41d5b0a582c37c6c
炫闪清灵 <128031688+xsql2023@users.noreply.github.com>
2023-04-08 11:36:17 +0800

    Create script.js
    Signed-off-by: 炫闪清灵 <128031688+xsql2023@users.noreply.github.com>

---
+++ index/script.js
@@ -0,0 +1,38 @@
+window.onload = function(){
+  document.getElementById('btn-1').addEventListener('click',goTo01)
+  document.getElementById('btn-2').addEventListener('click',goTo02)
+  document.getElementById('btn-3').addEventListener('click',goTo03)
+  document.getElementById('btn-4').addEventListener('click',goTo04)
+  document.getElementById('btn-5').addEventListener('click',goTo05)
+  document.getElementById('btn-6').addEventListener('click',goTo06)
+  document.getElementById('btn-7').addEventListener('click',goTo07)
+  document.getElementById('btn-8').addEventListener('click',goTo08)
+}
+
+function goTo01(){
+  window.open('https://sucloudplus.github.io/cibiao.html')
+}
+function goTo02(){
+  window.open('https://sucloudplus.github.io/baike.html')
+}
+function goTo03(){
+  window.open('http://www.baidu.com')
+}
+function goTo04(){
+  window.open('http://www.baidu.com')
+}
+function goTo05(){
+  window.open('http://www.baidu.com')
+}
+function goTo06(){
+  window.open('http://www.baidu.com')
+}
+function goTo07(){
+  window.open('http://www.baidu.com')
+}
+function goTo08(){
+  window.open('http://www.baidu.com')
+}
+function goTo09(){
+  window.open('http://www.baidu.com')
+}
